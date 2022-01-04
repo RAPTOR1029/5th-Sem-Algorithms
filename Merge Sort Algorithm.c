c3db85ac0ad1c82309c7e37643c5d1f67c274b3e
RAPTOR1029 <79849417+RAPTOR1029@users.noreply.github.com>
2022-01-04 12:49:47 +0530

    Add files via upload

--- Merge Sort Algorithm.c
+++ Merge Sort Algorithm.c
@@ -1,6 +1,68 @@
 #include<stdio.h>
-void mergesort(int a[],int b,int c);
-void merge(int a[],int l1,int u1, int u2);
+void merge(int a[],int l,int m,int n)
+{
+	int i,j,k;
+	int n1 = m - l + 1;
+    int n2 = n - m;
+	int L[n1],R[n2];
+	
+	for (i = 0; i < n1; i++)
+  L[i] = a[l + i];
+    for (j = 0; j < n2; j++)
+  R[j] = a[m + 1 + j];
+        
+	i=0;
+	j=0;
+	k=l;
+	
+    while (i < n1 && j < n2)
+ {
+    if (L[i] <= R[j]) {
+    a[k] = L[i];
+    i++;
+ }
+  else {
+    a[k] = R[j];
+    j++;
+}
+    k++;
+}
+  
+    while (i < n1) {
+	a[k] = L[i];
+	i++;
+    k++;
+    }
+  
+    while (j < n2) {
+    a[k] = R[j];
+    j++;
+    k++;
+    }
+}
+
+void printarr(int a[], int n)
+{
+int i;
+for (i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
+
+
+
+void mergesort(int a[], int l, int n)
+{
+	if(l<n)
+	{
+		int m=l+(n-l)/2;
+		mergesort(a,l,m);
+		mergesort(a,m+1,n);
+		merge(a,l,m,n);
+	}
+}
+
 int main()
 {
 	int a[100];
@@ -13,66 +75,13 @@ int main()
 	{
 		scanf("%d",&a[i]);
 	}
+	
+	printf("Array=");
+	printarr(a,n);
+	printf("\n");
 
-	mergesort(a,0,n);
+	mergesort(a,0,n-1);
 	
 	printf("Sorted array is:\n");
-	for (i=0;i<n;i++)
-	{
-		printf("%d",a[i]);
-	}
-}
-
-void mergesort(int a[], int l, int n)
-{
-	if(l<=n)
-	{
-		int mid=l+(n-l)/2;
-		mergesort(a,l,mid);
-		mergesort(a,mid+1,n);
-		merge(a,l,mid,n);
-	}
-}
-
-void merge(int a[],int l,int m,int n)
-{
-	int n1 = m - l + 1;
-    int n2 = n - m;
-	int L[n1],R[n2];
-
-	int i,j,k;
-	
-	for (i = 0; i < n1; i++)
-       { L[i] = a[l + i];}
-    for (j = 0; j < n2; j++)
-     { R[j] = a[l + 1 + j];}
-        
-        i=0,j=0;k=l;
-        while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            a[k] = L[i];
-            i++;
-        }
-        else {
-            a[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-  
-    /* Copy the remaining elements of L[], if there
-    are any */
-    while (i < n1) {
-        a[k] = L[i];
-        i++;
-        k++;
-    }
-  
-    /* Copy the remaining elements of R[], if there
-    are any */
-    while (j < n2) {
-        a[k] = R[j];
-        j++;
-        k++;
-    }
+	printarr(a,n);
 }
